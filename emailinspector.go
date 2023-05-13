2897781fe13dfef29752f4333077c9b48db4a71e
Zilehuda Tariq <zilehuda.tariq@gmail.com>
2023-05-13 09:36:49 +0500

    removed print statements.

--- emailinspector.go
+++ emailinspector.go
@@ -56,11 +56,7 @@ func IsValidEmail(email string) bool {
 }
 
 func HasValidMXRecords(domain string) bool {
-	mxrecords, err := net.LookupMX(domain)
-	for _, mx := range mxrecords {
-		fmt.Println(mx.Host, mx.Pref)
-	}
-
+	_, err := net.LookupMX(domain)
 	return err == nil
 }
 
@@ -68,7 +64,6 @@ func IsBlacklisted(domain string) bool {
 	for _, server := range dnsblServers {
 		query := fmt.Sprintf("%s.%s", domain, server)
 		ips, err := net.LookupIP(query)
-		fmt.Println(ips, err)
 		if err == nil && len(ips) > 0 {
 			return true
 		}
